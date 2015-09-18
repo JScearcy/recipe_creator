7ff3cc6b95e41fb28f9ea330208539c36ac9b644
JScearcy <jacobscearcy@gmail.com>
2015-09-18 17:34:59 -0500

    Added calc bar and 4 of 5 calcs function

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,10 +1,3 @@
-// requirejs.config({
-//   baseUrl: 'vendors/angularjs',
-//});
-
-//requirejs(['angular', 'angular-route', 'angular-animate', 'angular-aria', 'angular-material']);
-
-//function(angular, ngRoute, ngAnimate, ngAria, ngMaterial){
   var app = angular.module('recipeCreator', ['ngMaterial', 'ngRoute']);
   app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $locationProvider.html5Mode(true);
@@ -23,65 +16,115 @@
     }
   });
   app.controller('recipeMake', ['$scope', '$http', function($scope, $http){
-    getGrains();
-    getHops();
-    getYeasts();
-    $scope.addGrains = function(){
-      if(!$scope.addedGrains) {
-        $scope.addedGrains = {
-          Grains: [],
-          totalWeight: function() {
-            var total = 0;
-            $scope.addedGrains.Grains.forEach(function(grain, index) {
-              if(grain.grainWt > 0){
-                total += parseInt(grain.grainWt);
-              }
+    getItem('grains');
+    getItem('hops');
+    getItem('yeast');
+
+    $scope.recipe = {
+      name: 'Enter Name',
+      grains: {
+        totalWeight: function() {
+          var total = 0;
+          $scope.recipe.grains.added.forEach(function(grain, index) {
+            if(grain.grainWt > 0){
+              total += parseInt(grain.grainWt);
+            }
+          })
+          return total;
+        },
+        totalPPG: function(){
+          var total = 0;
+          $scope.recipe.grains.added.forEach(function(grain, index){
+            total += (Math.round((grain.PPG - 1) * 1000 * grain.grainWt / $scope.recipe.volume)) / 1000;
+          })
+          return total;
+        },
+        efficiencyPPG: function(){
+          return Math.round($scope.recipe.grains.totalPPG() * ($scope.recipe.efficiency / 100) * 1000) / 1000 + 1;
+        },
+        added: []
+      },
+      efficiency: 65,
+      attenuation: 70,
+      volume: 5,
+      hops: {
+        added: [],
+        ibu: function() {
+          if($scope.recipe.hops.added.length > 0) {
+            var aau = 0;
+            $scope.recipe.hops.added.forEach(function(hop) {
+              aau += hop.hopWt * hop.Alpha_Acid;
             });
-            return total;
+            
           }
-        };
-      }
-      $scope.addedGrains.Grains.push($scope.grain);
+          return "IBU's coming soon"
+        }
+      },
+      og: function() {
+        if($scope.recipe.grains.added.length > 0) {
+          return $scope.recipe.grains.efficiencyPPG();
+        } else {
+          return 0;
+        }
+      },
+      fg: function() {
+        if($scope.recipe.grains.added.length > 0) {
+          var yeastFood = Math.round((($scope.recipe.grains.efficiencyPPG() - 1) * 1000) * ($scope.recipe.attenuation / 100)) / 1000;
+          return $scope.recipe.og() - yeastFood;
+        } else {
+        return 0
+        }
+      },
+      abv: function() {
+        if($scope.recipe.grains.added.length > 0) {
+          var og = $scope.recipe.og(),
+              fg = $scope.recipe.fg();
+          return Math.round((76.08 * (og-fg) / (1.775 - og)) * (fg / 0.794) * 100) / 100;
+        } else {
+          return 0
+        }
+      },
+      srm: function() {
+        if($scope.recipe.grains.added.length > 0) {
+          var totalColor = 0;
+          var mcu = $scope.recipe.grains.added.forEach(function(grain, index) {
+            totalColor += (grain.lovi * grain.grainWt);
+          });
+          mcu = totalColor / $scope.recipe.volume;
+          return Math.round((1.4922 * (Math.pow(mcu, 0.6859))) * 100) / 100;
+        } else {
+          return 0;
+        }
+      },
+      dp: function() {
+        if($scope.recipe.grains.added.length > 0) {
+          var totaldp = 0;
+          $scope.recipe.grains.added.forEach(function(grain, index) {
+            totaldp += grain.dp * grain.grainWt;
+          });
+          return Math.round(totaldp / $scope.recipe.grains.totalWeight());
+        } else {
+          return 0;
+        }
+      },
+      notes: ''
+    };
+    $scope.addIngredient = function(ingredient, type){
+      $scope.recipe[ingredient].added.push($scope[type]);
       $scope.grain = {};
     }
-    $scope.addHops = function(){
-      if(!$scope.addedHops){
-        $scope.addedHops = [];
-      }
-      $scope.addedHops.push($scope.hop);
-      $scope.hop = {};
-    }
     $scope.deleteItem = function(index, type) {
-      if(type == 'grain'){
-        $scope.addedGrains.Grains.splice(index, 1);
-      } else if(type == 'hop'){
-        $scope.addedHops.splice(index,1);
-      }
+      $scope.recipe[type].added.splice(index, 1);
     }
-    function getGrains() {
+    //generic function to call the server for a specific ingredient
+    function getItem(item){
       $http({
         method: 'GET',
-        url: '/grains'
-      }).then(function(res){
-        $scope.grains = res.data;
-      });
-    }
-    function getHops() {
-      $http({
-        method: 'GET',
-        url: '/hops'
-      }).then(function(res){
-        $scope.hops = res.data;
-      });
-    }
-    function getYeasts() {
-      $http({
-        method: 'GET',
-        url: '/yeast'
-      }).then(function(res){
-        $scope.yeasts = res.data;
-      });
-    }
+        url: '/' + item
+      }).then(function(res) {
+        $scope[item] = res.data;
+      })
+    };
   }]);
   app.controller('dbsgCalc', ['$scope', function($scope){
     console.log('clicked');
@@ -94,4 +137,3 @@
     //   $scope.grain = {};
     // }
   }]);
-//};
